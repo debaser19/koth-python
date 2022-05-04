285b301c1d0c9e40b77cf33751bc686016c92721
debaser19 <steve@stoveld.xyz>
2022-05-04 11:57:10 -0400

    mmr lookup changes
    fix mmr lookup so it will look back at previous seasons to find most recent mmr if user does not have any game played in current season

--- app.py
+++ app.py
@@ -11,6 +11,17 @@ app.config.update(TEMPLATES_AUTO_RELOAD=True)
 mongo = PyMongo(app)
 
 
+def get_mmr(user, race):
+    season = 11
+    while season > 0:
+        res = requests.get(config.w3c_url + user.replace("#", "%23") + f'/game-mode-stats?gateWay=20&season={season}')
+        for item in res.json():
+            if item['gameMode'] == 1 and item['race'] == int(race):
+                return int(item["mmr"])
+        season -= 1
+    return None
+
+
 @app.route('/', methods=['POST', 'GET'])
 def list_brackets():
     users = list(mongo.db.koths.find())
@@ -57,31 +68,32 @@ def signup_user():
 
 @app.route('/add_users', methods=['POST', 'GET'])
 def add_user():
-    username = request.form['username'].replace('#', '%23')
+    username = request.form['username']
     race = request.form['race']
-    res = requests.get(config.w3c_url + username + '/game-mode-stats?gateWay=20&season=11')
-    for item in res.json():
-        if item['gameMode'] == 1 and item['race'] == int(race):
+    # res = requests.get(config.w3c_url + username + '/game-mode-stats?gateWay=20&season=11')
+    # for item in res.json():
+    #     if item['gameMode'] == 1 and item['race'] == int(race):
             # determine bracket based on mmr
-            if int(item['mmr']) > 1600:
-                bracket = 1
-            elif int(item['mmr']) < 1450:
-                bracket = 3
-            else:
-                bracket = 2
-            user = {
-                'username': username.replace('%23', '#'),
-                'race': int(race),
-                'mmr': int(item["mmr"]),
-                'bracket': bracket,
-                'is_king': False
-            }
+    mmr = get_mmr(username, race)
+    if int(mmr) > 1600:
+        bracket = 1
+    elif int(mmr) < 1450:
+        bracket = 3
+    else:
+        bracket = 2
+    user = {
+        'username': username.replace('%23', '#'),
+        'race': int(race),
+        'mmr': int(mmr),
+        'bracket': bracket,
+        'is_king': False
+    }
 
-            try:
-                mongo.db.koths.insert_one(user)
-                print('Added document to db')
-            except Exception as e:
-                print(f'Error adding document: {e}')
+    try:
+        mongo.db.koths.insert_one(user)
+        print('Added document to db')
+    except Exception as e:
+        print(f'Error adding document: {e}')
 
     return redirect(request.referrer)
 
@@ -116,31 +128,32 @@ def manual_add_user():
 
 @app.route('/new_signup', methods=['POST', 'GET'])
 def user_signup():
-    username = request.form['username'].replace('#', '%23')
+    username = request.form['username']
     race = request.form['race']
-    res = requests.get(config.w3c_url + username + '/game-mode-stats?gateWay=20&season=11')
-    for item in res.json():
-        if item['gameMode'] == 1 and item['race'] == int(race):
+    # res = requests.get(config.w3c_url + username + '/game-mode-stats?gateWay=20&season=11')
+    # for item in res.json():
+    #     if item['gameMode'] == 1 and item['race'] == int(race):
             # determine bracket based on mmr
-            if int(item['mmr']) > 1600:
-                bracket = 1
-            elif int(item['mmr']) < 1450:
-                bracket = 3
-            else:
-                bracket = 2
-            user = {
-                'username': username.replace('%23', '#'),
-                'race': int(race),
-                'mmr': int(item["mmr"]),
-                'bracket': bracket,
-                'is_king': False
-            }
+    mmr = get_mmr(username, race)
+    if int(mmr) > 1600:
+        bracket = 1
+    elif int(mmr) < 1450:
+        bracket = 3
+    else:
+        bracket = 2
+    user = {
+        'username': username.replace('%23', '#'),
+        'race': int(race),
+        'mmr': int(mmr),
+        'bracket': bracket,
+        'is_king': False
+    }
 
-            try:
-                mongo.db.koths.insert_one(user)
-                print('Added document to db')
-            except Exception as e:
-                print(f'Error adding document: {e}')
+    try:
+        mongo.db.koths.insert_one(user)
+        print('Added document to db')
+    except Exception as e:
+        print(f'Error adding document: {e}')
 
     return redirect(request.referrer)
 
